48941f7d625df826eaaee467e39a72f7e188b7f0
Ibrahim-nassih <binni3673@gmail.com>
2024-08-11 11:08:43 -0700

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,17 @@
 #!/usr/bin/node
 
-const req = require('request');
-const util = require('util');
+const request = require('request');
 
-// Get the film ID
-const film_Id = process.argv[2];
-
-// Construct the URL
-const URL = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
-
-// Promisify the request function
-const prequest = util.promisify(req);
-
-(async () => {
-  try {
-    // Fetch the film details
-    const film = (await prequest(URL, { json: true })).body;
-
-    // Iterate over the list of character URLs
-    for (const url of film.characters) {
-      // Fetch each character's details
-      const char = (await prequest(url, { json: true })).body;
-      // Print the character's name
-      console.log(char.name);
-    }
-  } catch (error) {
-    // Handle error
-    console.log(error);
-  }
-})();
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
